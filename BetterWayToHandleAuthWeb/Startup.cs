c51b3179491260895604595bc686cd959ed1ad4c
dadwonkulahjr <dad.wonkulah@comaxict.com>
2021-12-01 15:36:31 +0000

    Remove unnecessary namespace and default comments from in the startup.cs file.

--- BetterWayToHandleAuthWeb/Startup.cs
+++ BetterWayToHandleAuthWeb/Startup.cs
@@ -4,10 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+
 
 namespace BetterWayToHandleAuthWeb
 {
@@ -20,13 +17,11 @@ namespace BetterWayToHandleAuthWeb
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -36,7 +31,6 @@ namespace BetterWayToHandleAuthWeb
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
